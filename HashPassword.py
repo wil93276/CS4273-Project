7363964ca1588d85c30bbb80c3ca001749379871
CSullard <89896043+CSullard@users.noreply.github.com>
2023-12-03 17:24:10 -0600

    Update HashPassword.py

--- HashPassword.py
+++ HashPassword.py
@@ -4,4 +4,5 @@ import hashlib
 def HashPassword(password):
     # Using the hashlib library, using the sha256 algorithm to hash a given password
     sha256 = hashlib.sha256()
-    return sha256.update(password.encode()) # Updated method takes encoded data as an argument
+    sha256.update(password.encode()) # Method takes encoded data as an argument
+    return sha256.hexdigest() # returns a string object of double length that contains hexadecimal digits
