9ab4eedffb686bb66669dedf0dd84caa69536b15
CSullard <89896043+CSullard@users.noreply.github.com>
2023-12-03 14:58:01 -0600

    Update HashPassword.py

--- HashPassword.py
+++ HashPassword.py
@@ -1,9 +1,8 @@
 import shelve
 import hashlib
-# Using the hashlib library, using the sha256 algorithm to hash a given password
-sha256 = hashlib.sha256()
 # Return hashed password
 def HashPassword(password):
-    hasher = hashlib.sha256()
-    hasher.update(password.encode()) #update method takes encoded data as argument
+    # Using the hashlib library, using the sha256 algorithm to hash a given password
+    sha256 = hashlib.sha256()
+    sha256.update(password.encode()) # Updated method takes encoded data as an argument
     return password
